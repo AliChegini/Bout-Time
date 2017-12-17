0eeff2249a9a4c1b2cab81fe23a69f3890c845df
Ali Chegini <ali.c.engineer@gmail.com>
2017-12-17 02:25:49 +0100

    improved and working UI logic

--- BoutTime/ViewController.swift
+++ BoutTime/ViewController.swift
@@ -11,10 +11,18 @@ import UIKit
 class ViewController: UIViewController {
     
     
-    @IBOutlet weak var event1: UILabel!
-    @IBOutlet weak var event2: UILabel!
-    @IBOutlet weak var event3: UILabel!
-    @IBOutlet weak var event4: UILabel!
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var label4: UILabel!
+    
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
     
     
     var dictionary: [String: Any] = [:]
@@ -43,16 +51,66 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func moveDownLabel1() {
+        button1.setImage(#imageLiteral(resourceName: "down_full_selected"), for: .highlighted)
+        let temp = label1.text
+        label1.text = label2.text
+        label2.text = temp
+    }
+    
+    
+    @IBAction func moveUpLabel2() {
+        button2.setImage(#imageLiteral(resourceName: "up_half_selected"), for: .highlighted)
+        let temp = label2.text
+        label2.text = label1.text
+        label1.text = temp
+    }
+    
+   
+    @IBAction func moveDownLabel2() {
+        button3.setImage(#imageLiteral(resourceName: "down_half_selected"), for: .highlighted)
+        let temp = label2.text
+        label2.text = label3.text
+        label3.text = temp
+    }
+    
+    
+    
+    @IBAction func moveUpLabel3() {
+        button4.setImage(#imageLiteral(resourceName: "up_half_selected"), for: .highlighted)
+        let temp = label3.text
+        label3.text = label2.text
+        label2.text = temp
+    }
+    
+    @IBAction func moveDownLabel3() {
+        button5.setImage(#imageLiteral(resourceName: "down_half_selected"), for: .highlighted)
+        let temp = label3.text
+        label3.text = label4.text
+        label4.text = temp
+    }
+    
+    @IBAction func moveUpLabel4() {
+        button6.setImage(#imageLiteral(resourceName: "up_full_selected"), for: .highlighted)
+        let temp = label4.text
+        label4.text = label3.text
+        label3.text = temp
+    }
+    
+    
     
     // Display events
     func displayEvents() {
         
         let eventPack = EventPack.provideEventPack(organizedEvents)
-        event1.text = eventPack.event1.event
-        event2.text = eventPack.event2.event
-        event3.text = eventPack.event3.event
-        event4.text = eventPack.event4.event
+        label1.text = eventPack.event1.event
+        label2.text = eventPack.event2.event
+        label3.text = eventPack.event3.event
+        label4.text = eventPack.event4.event
     }
+    
+    
+    
 
 }
 
